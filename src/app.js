8d1ac3ed61cc3fe8caf602fbfbfb69eef07768dc
ujc01 <30632545+ujc01@users.noreply.github.com>
2020-06-19 10:41:19 +0530

    added test cases

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
+import 'react-dates/lib/css/_datepicker.css'
 import 'normalize.css'
 import './styles/styles.scss'
+
 
 const store = configureStore()
 
--- src/tests/components/ExpenseList.test.js
+++ src/tests/components/ExpenseList.test.js
@@ -8,3 +8,7 @@ test('should render ExpenseList with expenses', () => {
     expect(wrapper).toMatchSnapshot()
 })
 
+test('should render ExpenseList with empty message', () => {
+    const wrapper = shallow(<ExpenseList expenses={[]}/>)
+    expect(wrapper).toMatchSnapshot()
+})
